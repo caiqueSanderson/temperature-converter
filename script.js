9b005965b3ecd84f7efb67e4242cefd1f16c2f61
sanderson <cbsanderson.04@gmail.com>
2024-10-10 14:14:43 -0300

    updates and corrections

--- script.js
+++
@@ -1,9 +0,0 @@
-const frm = document.querySelector('form');
-const resposta = document.querySelector('h3');
-
-frm.addEventListener('submit', (e) => {
-  const numero = frm.inNumber.value;
-  const numeroConvertido = (numero * 9/5) + 32;
-  resposta.innerText = `O valor de Celsius convertido em Fahrenheit é: ${numeroConvertido}`;
-  e.preventDefault();
-})
